7befbbbeee7b1f6210811b028de8ef4bd53acbb9
Kacper Harasim <kacper.harasim@polidea.com>
2016-05-09 22:48:33 +0200

    Redirecting outputs to file paths.

--- Sources/Node/ProcessRunner.cpp
+++ Sources/Node/ProcessRunner.cpp
@@ -3,16 +3,27 @@
 //
 
 #include <cstdlib>
+#include <fcntl.h>
 #include "ProcessRunner.h"
 
-void ProcessRunner::runProcessAtPath(std::string processFilePath) {
+void ProcessRunner::runProcessAtPath(std::string processFilePath,
+                                     std::string processErrorPath,
+                                     std::string processStdOutputPath) {
   pid_t newProcessId;
+  int childProcessStatus;
   if ((newProcessId = fork()) < 0) {
     exit(-1);
   } else if (newProcessId == 0) {
+    char *params[4]  = {0};
+    int stdoutFd = open(processStdOutputPath.c_str(), O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+    int stdErrFd = open(processErrorPath.c_str(), O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
 
+    dup2(stdoutFd, 1);
+    dup2(stdErrFd, 2);
+
+    execvp(processFilePath.c_str(), params);
   } else {
-    
+    wait(&childProcessStatus);
   }
 
 }
