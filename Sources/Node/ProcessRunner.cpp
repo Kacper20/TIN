08b2226e8b8495f3838e6a8aedb12f16172c04dd
Kacper Harasim <kacper.harasim@polidea.com>
2016-05-09 22:14:58 +0200

    Process runner scaffold.

--- Sources/Node/ProcessRunner.cpp
+++ Sources/Node/ProcessRunner.cpp
@@ -2,4 +2,17 @@
 // Created by Kacper Harasim on 09.05.2016.
 //
 
+#include <cstdlib>
 #include "ProcessRunner.h"
+
+void ProcessRunner::runProcessAtPath(std::string processFilePath) {
+  pid_t newProcessId;
+  if ((newProcessId = fork()) < 0) {
+    exit(-1);
+  } else if (newProcessId == 0) {
+
+  } else {
+    
+  }
+
+}
--- Sources/Node/ProcessRunner.h
+++ Sources/Node/ProcessRunner.h
@@ -7,6 +7,8 @@
 
 
 #include <string>
+#include <unistd.h>
+
 class ProcessRunner {
 
  public:
