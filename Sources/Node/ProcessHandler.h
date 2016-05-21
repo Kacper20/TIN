90ca977998dd7f8ede4bd06f19eb9ce261bc5d44
Kacper Harasim <kacper.harasim@polidea.com>
2016-05-21 23:14:22 +0200

    Fix.

--- Sources/Node/ProcessHandler.h
+++ Sources/Node/ProcessHandler.h
@@ -8,12 +8,20 @@
 
 #include <string>
 #include <unistd.h>
+#include <set>
 #include "../Shared/MessagesQueue.h"
 #include "../Shared/Commands/StartProcessCommand.h"
 
 //Class that handles all of the interaction with processes that are computing data.
-#define PROCESSES_BASE_DIRECTORY "/Users/kacperh/Developer/TIN_TEMP"
 
+typedef std::tuple<std::shared_ptr<StartProcessCommand>, int> CommandWithPid;
+
+struct CommandWithPidCompare {
+  bool operator() (const CommandWithPid& lhs, const CommandWithPid& rhs) const
+  {
+    return std::get<1>(lhs) < std::get<1>(rhs);
+  }
+};
 
 //Instance that is
 class ProcessHandler {
@@ -24,7 +32,7 @@ class ProcessHandler {
   void writeProcessToPersistentStorage(Json::Value commandJson, const std::string& processContent,
                                        const std::string& identifier);
   void runProcessWithCommand(std::shared_ptr<StartProcessCommand> command, const std::string& basePath);
-  std::queue<std::tuple<std::shared_ptr<StartProcessCommand>, int> > runningProcessesQueue;
+  std::set<CommandWithPid, CommandWithPidCompare> runningProcesses;
   MessagesQueue<StartProcessCommand> processesToRunQueue;
 
  public:
