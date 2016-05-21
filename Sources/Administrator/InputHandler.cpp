8012a974097b20f136bb7958ab4ba7e96459f542
Daria Danilenko <dariadanilenko3@gmail.com>
2016-05-22 00:51:08 +0200

    changes according to comments on PR

--- Sources/Administrator/InputHandler.cpp
+++ Sources/Administrator/InputHandler.cpp
@@ -5,83 +5,86 @@
 #include "InputHandler.h"
 #include <iostream>
 #include <cstring>
+using namespace std;
 
 void InputHandler::run()
 {
-    std::cout << "Waiting for a command:\n";
-    std::string command;
-    std::string name;
+  cout << "Waiting for a command:\n";
+  string command;
+  string name;
 
-    while ( name != "exit")
-    {
-        std::getline (std::cin,command);
+  do
+  {
+    getline (cin,command);
 
-        if ( command.find(" ") == std::string::npos )
-            name = command;
-        else name = command.substr(0,command.find_first_of(" "));
-
-        if ( name == "help") {
-        printHelp();
-        }
-        else if ( name == "connect") {
-        handleConnect(command);
-        }
-        else if ( name == "disconnect") {
-        handleDisconnect();
-        }
-        else if ( name == "send_process" ) {
-        sendProcess(command);
-        }
-        else if (  name == "launch_process" ) {
-        launchProcess(command);
-        }
-        else {
-        std::cout << "Incorrect command.\n";
-        printHelp();
-        }
+    if ( command.find(" ") == string::npos ) {
+      name = command;
     }
-    std::cout << "Exiting Administrator process. Goodbye.\n";
+    else {
+      name = command.substr(0,command.find_first_of(" "));
+    }
+
+    if ( name == "help") {
+      printHelp();
+    }
+    else if ( name == "connect") {
+      handleConnect(command);
+    }
+    else if ( name == "disconnect") {
+      handleDisconnect();
+    }
+    else if ( name == "send_process" ) {
+      sendProcess(command);
+    }
+    else if (  name == "launch_process" ) {
+      launchProcess(command);
+    }
+    else {
+      std::cout << "Incorrect command.\n";
+      printHelp();
+    }
+  } while ( name != "exit");
+
+  cout << "Exiting Administrator process. Goodbye.\n";
 }
 
 
 void InputHandler::printHelp()
 {
-    std::cout << "Available commands for administrator process:" << "\n";
-    std::cout << "connect <server address:port> - Connects to server on the specified address." << "\n";
-    std::cout << "send_process <name> <path to file> - Sends the file you choose to the server" << "\n";
-    std::cout << "\t and saves it to the server with the name specified in 'name' argument" << "\n";
-    std::cout << "launch_process <name> - Tells the server to start chosen process." << "\n"; // Needs improvement
-    std::cout << "disconnect - Disconnects from the server." << "\n";
-    std::cout << "help - Shows this message." << "\n";
-    std::cout << "exit - Exits from the administrator process, closes connections." << "\n";
+  cout << "Available commands for administrator process:" << "\n";
+  cout << "connect <server address:port> - Connects to server on the specified address." << "\n";
+  cout << "send_process <name> <path to file> - Sends the file you choose to the server" << "\n";
+  cout << "\t and saves it to the server with the name specified in 'name' argument" << "\n";
+  cout << "launch_process <name> - Tells the server to start chosen process." << "\n"; // Needs improvement
+  cout << "disconnect - Disconnects from the server." << "\n";
+  cout << "help - Shows this message." << "\n";
+  cout << "exit - Exits from the administrator process, closes connections." << "\n";
 }
 
-int InputHandler::checkAddress(std::string address_with_port)
+int InputHandler::checkAddress(const string& address_with_port)
 {
-    return 0;
+  return 0;
 }
 
-void InputHandler::handleConnect(std::string full_command)
+void InputHandler::handleConnect(const string& full_command)
 {
-    if( full_command.length() <= strlen("connect"))
-    {
-        std::cout << "Invalid command. The correct call is: connect <server address:port>. \n";
-        std::cout << "Please try again. \n";
-        return;
-    }
+  if( full_command.length() <= strlen("connect")) {
+    cout << "Invalid command. The correct call is: connect <server address:port>. \n";
+    cout << "Please try again. \n";
+    return;
+  }
 
-    std::string address_with_port = full_command.substr(full_command.find_first_of(" ")+1);
-    int result = checkAddress(address_with_port);
+  string address_with_port = full_command.substr(full_command.find_first_of(" ")+1);
+  int result = checkAddress(address_with_port);
 
-    if(result == 0)
-    {
+  if(result == 0) {
         //TODO - start connection to server here
-    }
-    else
-    {
-        std::cout << "Invalid address. The correct way is for example: 111.111.111.111:20.\n";
-        std::cout << "Please try again.\n";
-    }
+  }
+  else
+  {
+    cout << "Invalid address. The correct way is for example: 111.111.111.111:20.\n";
+    cout << "Please try again.\n";
+  }
 }
 
 void InputHandler::handleDisconnect()
@@ -89,16 +92,15 @@ void InputHandler::handleDisconnect()
 
 }
 
-void InputHandler::sendProcess(std::string full_command)
+void InputHandler::sendProcess(const string& full_command)
 {
-    if( full_command.length() <= strlen("send_process"))
-    {
-        std::cout << "Invalid command. The correct call is: send_process <name> <path to file>. \n";
-        std::cout << "Please try again. \n";
-        return;
-    }
+  if( full_command.length() <= strlen("send_process")) {
+    cout << "Invalid command. The correct call is: send_process <name> <path to file>. \n";
+    cout << "Please try again. \n";
+    return;
+  }
 
-    std::string process_name = full_command.substr(full_command.find_first_of(" ")+1);
+  string process_name = full_command.substr(full_command.find_first_of(" ")+1);
 
     //TODO check if there is correct nr of arguments + check if file exists
 
@@ -106,16 +108,15 @@ void InputHandler::sendProcess(std::string full_command)
 
 }
 
-void InputHandler::launchProcess(std::string full_command)
+void InputHandler::launchProcess(const string& full_command)
 {
-    if( full_command.length() <= strlen("launch_process"))
-    {
-        std::cout << "Invalid command. The correct call is: launch_process <name>. \n";
-        std::cout << "Please try again. \n";
-        return;
-    }
+  if( full_command.length() <= strlen("launch_process")) {
+    cout << "Invalid command. The correct call is: launch_process <name>. \n";
+    cout << "Please try again. \n";
+    return;
+  }
 
-    std::string process_name = full_command.substr(full_command.find_first_of(" ")+1);
+  string process_name = full_command.substr(full_command.find_first_of(" ")+1);
 
     //TODO send process_name to server to launch
 }
