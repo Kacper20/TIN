d554f29ee5ca35836d2843142ad90e6ef2daf441
Daria Danilenko <dariadanilenko3@gmail.com>
2016-05-22 18:39:34 +0200

    test commit

--- Sources/Administrator/AdministratorNetworkLayer.h
+++ Sources/Administrator/AdministratorNetworkLayer.h
@@ -5,4 +5,26 @@
 #ifndef TIN_ADMINISTRATORNETWORKLAYER_H
 #define TIN_ADMINISTRATORNETWORKLAYER_H
 
+
+#include <functional>
+#include "../Shared/TCPSocket.h"
+#include "../Shared/MessageNetworkManager.h"
+
+#define NETWORK_ADMIN_PORT 46028
+
+
+class AdminNetworkLayer
+{
+private:
+    TCPSocket serverConnectionSocket;
+    MessageNetworkManager networkManager;
+
+public:
+    AdminNetworkLayer() : serverConnectionSocket(TCPSocket()), networkManager(serverConnectionSocket) {}
+    int connectToServer(const std::string addressWithPort);
+    int sendMessage(const std::string message) const;
+    int disconnectFromServer();
+};
+
+
 #endif //TIN_ADMINISTRATORNETWORKLAYER_H
